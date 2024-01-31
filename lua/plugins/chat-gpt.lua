26ab73327eaa51f79d4c15cc9cd43dbfc010d1fc
lobov.a <lobov.a@teledoc.ru>
2024-01-31 14:34:11 +0300

    initial commit

---
+++ lua/plugins/chat-gpt.lua
@@ -0,0 +1,31 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup {
+      api_key_cmd = "echo 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 ",
+      openai_params = {
+        model = "gpt-3.5-turbo",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-3.5-turbo",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    }
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
---
+++ lua/plugins/styled-components.lua
@@ -0,0 +1,4 @@
+return {
+  "styled-c:so ~/.vimrcomponents/vim-styled-components",
+  branch = "main",
+}
