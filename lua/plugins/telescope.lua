53b1c8a59d6113efac4ddfc02a13919d2ed607e0
lobov.a <lobov.a@teledoc.ru>
2026-01-20 15:00:23 +0300

    refactor: фикс ошибки telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,15 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
+  branch = "master", -- вместо tag = "0.1.8"
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup {}
+    require("telescope").setup {
+      defaults = {
+        preview = {
+          treesitter = false, -- отключает ошибку ft_to_lang
+        },
+      },
+    }
     local builtin = require "telescope.builtin"
     vim.keymap.set("n", "<leader>ff", function()
       builtin.find_files { hidden = true }
